447345176c417819639df11f10d794a8f919ceef
María Paula Vargas Rincón <mariapaulavargasrincon@MacBook-Air-de-Maria.local>
2023-10-31 19:29:57 -0500

    version inicial del parcial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+      <h1>Carga de estudiantes nueva nueva</h1>
+      <form></form>
+      <Card/>
+      </div>
+    </>
+  )
+}
+
+export default App
